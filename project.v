6676b26077aa8437906c1b03c361814621f4abbb
Jocelyn Lau <jocelyn.ty.lau@gmail.com>
2025-09-13 12:04:19 -0400

    Update project.v

--- project.v
+++ project.v
@@ -8,7 +8,7 @@ module flappy_bird_top (
     output wire [7:0] uo_out,  //Dedicated outputs
     input  wire [7:0] uio_in,  //IOs: Input path
     output wire [7:0] uio_out, //IOs: Output path
-    output wire [7:0] uio_ou,  //IOs: Enable path (active high: 0=input, 1=output)
+    output wire [7:0] uio_oe,  //IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,     //always 1 when design is powered
     input  wire       clk,     //clock
     input  wire       rst_n    //reset: low to reset
